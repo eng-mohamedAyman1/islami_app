11006edf3e2963b281eb2a259a52b32a57e0a8a2
mohamed ayman <>
2026-05-14 21:55:15 +0300

    feat(BottomNavigationBar): implement BottomNavigationBar screens
    - Added interactive BottomNavigationBar screens
    - Integrated SVG asset support
    - Improved first-time user experience

--- lib/ul/home_screen.dart
+++ lib/ul/home_screen.dart
@@ -1,12 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:islami_app/util/app_assets.dart';
+import 'package:islami_app/util/app_colors.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+import 'tabBodyHomeScreen/tabAzkar.dart';
+import 'tabBodyHomeScreen/tabHadith.dart';
+import 'tabBodyHomeScreen/tabQuran.dart';
+import 'tabBodyHomeScreen/tabRadio.dart';
+import 'tabBodyHomeScreen/tabTime.dart' show Tabtime;
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+int currentIndex = 0;
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> bgScreen = [
+    AppImages.bgQuran,
+    AppImages.bgHadith,
+    AppImages.bgAzkar,
+    AppImages.bgRadio,
+    AppImages.bgTime,
+  ];
+  List<Widget> bodyScreen = [
+    Tabquran(),
+    Tabhadith(),
+    Tabazkar(),
+    Tabradio(),
+    Tabtime(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Image.asset("assets/images/introduction1.png"),
+    return SafeArea(
+      child: Stack(
+        children: [
+          Image.asset(bgScreen[currentIndex],fit:BoxFit.fill ,width: double.infinity,height: double.infinity,),
+          Scaffold(
+            bottomNavigationBar: Theme(
+              data: Theme.of(context).copyWith(
+                canvasColor: AppColors.white
+              ),
+              child: BottomNavigationBar(
+      
+                selectedItemColor: AppColors.white,
+                unselectedItemColor: AppColors.darkGray,
+                onTap: (index) {
+                  currentIndex = index;
+                  setState(() {});
+                },
+                currentIndex: currentIndex,
+                type: BottomNavigationBarType.fixed,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: BarItem(imagePass: AppIcons.icTabQuran, index: 0),
+                    label: "Quran",
+                  ),
+                  BottomNavigationBarItem(
+                    icon:  BarItem(imagePass: AppIcons.icTabHadith, index: 1),
+                    label: "Hadith",
+                  ),
+                  BottomNavigationBarItem(
+                    icon:  BarItem(imagePass: AppIcons.icTabAzkar, index: 2),
+                    label: "Azkar",
+                  ),
+                  BottomNavigationBarItem(
+                    icon:  BarItem(imagePass: AppIcons.icTabRadio, index: 3),
+                    label: "Radio",
+                  ),
+                  BottomNavigationBarItem(
+                    icon:  BarItem(imagePass: AppIcons.icTabTime, index: 4),
+                    label: "Time",
+                  ),
+                ],
+                backgroundColor: AppColors.gold,
+              ),
+            ),
+            body: Column(
+              children: [
+                 Image.asset("assets/images/Group 42.png"),
+                 Expanded(child: bodyScreen[currentIndex])
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BarItem extends StatelessWidget {
+  String imagePass;
+  int index;
+
+  BarItem({required this.imagePass, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return currentIndex == index ?Container(
+      decoration: BoxDecoration(
+        color: AppColors.mediumDarkGray,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical:6,
+      ),
+      child: SvgPicture.asset(
+        imagePass,
+        colorFilter: currentIndex == index? const ColorFilter.mode(
+        AppColors.white,
+        BlendMode.srcIn,
+      )
+          : null,
+      ),
+    ):SvgPicture.asset(
+    imagePass,
     );
   }
 }
